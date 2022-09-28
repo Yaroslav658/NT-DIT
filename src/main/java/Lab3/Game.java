4d5f50389572f5b2cf85fa0e0525392e397b4abe
Yaroslav Bondarenko <0985294686@mail.ru>
2022-09-28 21:32:04 +0300

    Refactor

--- src/main/java/Lab3/Game.java
+++ src/main/java/Lab3/Game.java
@@ -6,31 +6,14 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("enter name of 1st player: ");
+        System.out.print("Enter name of 1st player: ");
         Player player1 = new Player(scan.nextLine());
-        System.out.print("enter name of 2nd player: ");
+        System.out.print("Enter name of 2nd player: ");
         Player player2 = new Player(scan.nextLine());
         Field a = new Field(player1, player2);
-        a.startGame();
+        a.gameOptions();
         System.out.println();
         System.out.println(player1.getInformation());
         System.out.println(player2.getInformation());
-
-        System.out.println("Try again?");
-//        if (Objects.equals(scan.nextInt(), 1)){
-//            a.startGame();
-//        }
-
-//        System.out.println("        |           |       ");
-//        System.out.println("    O   |           |   X   ");
-//        System.out.println("        |           |       ");
-//        System.out.println("----------------------------");
-//        System.out.println("        |           |       ");
-//        System.out.println("    O   |           |   X   ");
-//        System.out.println("        |           |       ");
-//        System.out.println("----------------------------");
-//        System.out.println("        |           |       ");
-//        System.out.println("    O   |           |   X   ");
-//        System.out.println("        |           |       ");
     }
 }
